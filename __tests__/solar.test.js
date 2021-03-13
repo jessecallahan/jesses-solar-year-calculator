1754dddd6a94425dacc1c7e7a76e826e7d528cd5
Jesse Callahan <jessetylercallahan@gmail.com>
2021-03-13 11:19:35 -0800

    instantiated object passes first test

--- __tests__/solar.test.js
+++ __tests__/solar.test.js
@@ -1,4 +1,19 @@
+import { User } from '../src/Solar.js'
 
+describe('User', () => {
+  let testUser;
+
+  beforeEach(() => {
+    testUser = new User(10, "healthy")
+  })
+
+  test("should instantiate a new User with their age", () => {
+    expect(testUser.age).toEqual(10)
+  })
+
+
+
+});
 
 
 
